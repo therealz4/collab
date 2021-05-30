8d0f2d004d6c5ec6862d5e4d13ff40d064aa19f6
ChaosTempest812 <85038572+ChaosTempest812@users.noreply.github.com>
2021-05-30 19:30:58 -0400

    Delete REQUIREMENTS.py

--- REQUIREMENTS.py
+++
@@ -1,8 +0,0 @@
-  # Must have pip downloaded
-commands = ["pip install pygame"]
-
-# If pip is downloaded, check the current version
-commands = ["pip --version"]
-
-# If your pip is below the requirement (see prerequisites.py), update pip
-commands = ["pip install --upgrade pip"]
