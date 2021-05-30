a21033aeca092cb8b955f9aaa5d881a85f8b151d
ChaosTempest812 <85038572+ChaosTempest812@users.noreply.github.com>
2021-05-30 10:20:38 -0400

    Create REQUIREMENTS.py

---
+++ REQUIREMENTS.py
@@ -0,0 +1,9 @@
+
+# Must have pip downloaded
+commands = ["pip install pygame"]
+
+# If pip is downloaded, check the current version
+commands = ["pip --version"]
+
+# If your pip is below the requirement (see prerequisites.py), update pip
+commands = ["pip install --upgrade pip"]
